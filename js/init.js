ecbc19f33b3b16555e229d11b3d0d7a01f36d0db
revChess <130122303+revChess@users.noreply.github.com>
2023-04-07 18:00:11 -0400

    edit

--- js/init.js
+++ js/init.js
@@ -2,7 +2,7 @@
 
 (async ()=>{
     // thanks a lot Chromium
-    console.log(await navigator.storage.persisted());
+    console.log(`Has persistent storage: ${await navigator.storage.persisted()}`);
     if (!(await navigator.storage.persisted())) {
         promptUser(
             `Would you like to persist storage? It is currently not persisted. <b>Not persisting storage may result in an irrevocable loss of data.</b> Browsers may periodically clean up data that remains unused when there is low space on your computer, but there's some magic code that can request for it to persist and not get affected by this.<br><br>revChess does not store any user's chess games, Revs, or otherwise on a server. It is only stored locally.<br><br>On Chromium-based browsers, there is no prompt to accept this. Upon pressing 'okay' at the bottom of this alert, an attempt will be made to request persistent storage. If it fails, you will be alerted.<br><br><b>revChess is completely open sourced, and you can view all the code at github.com/revChess.</b>`,
@@ -14,10 +14,10 @@
                         ['I understand', 'Reload', 'Notifications'],
                         async response=>{
                             if (response === 'Reload') {
-                                location.reload();
+                                setTimeout(()=>{location.reload()}, 3000);
                             } else if (response === 'Notifications') {
                                 await Notification.requestPermission();
-                                location.reload();
+                                setTimeout(()=>{location.reload()}, 3000);
                             }
                         }
                     );
